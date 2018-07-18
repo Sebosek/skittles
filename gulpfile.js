fd191c662bec9e2bd3a989bbb001a302ca0ca043
Sebastian Bušek <sebastian.busek@konicaminolta.cz>
2018-07-18 12:41:40 +0200

    * Incrementail changes

--- gulpfile.js
+++ gulpfile.js
@@ -2,39 +2,56 @@
 
 var gulp = require('gulp'),
     sass = require('gulp-sass'),
+    copy = require('gulp-copy'),
     sourcemaps = require('gulp-sourcemaps'),
     minify = require('gulp-minify-css'),
     rename = require('gulp-rename'),
     json = require('./package.json'),
-    run = require('gulp-run');
+    run = require('gulp-run-command').default;
+
+const SRC_BASE = './src/scss',
+      SRC_ALL = `${SRC_BASE}/**/*.scss`,
+      SRC_COMPONENTS = `${SRC_BASE}/components.scss`,
+      DEST = './build/css';
 
 gulp.task('sass', function () {
-    return gulp.src([
-            './scss/**/*.scss'
-        ])
+    return gulp.src([SRC_ALL])
         .pipe(sass().on('error', sass.logError))  
         .pipe(sourcemaps.write())
-        .pipe(gulp.dest('./css'));
+        .pipe(gulp.dest(DEST));
 });
 
 gulp.task('sass:build', function () {
-    return gulp.src('./scss/**/*.scss')
+    return gulp.src(SRC_ALL)
         .pipe(sass().on('error', sass.logError))  
         .pipe(minify())
-        .pipe(rename(`${json.name}-${json.version}#${new Date().getTime()}.min.css`))
-        .pipe(gulp.dest('./css'));
+        .pipe(rename(`${json.name}-${json.version}.min.css`))
+        .pipe(gulp.dest(DEST));
 });
 
 gulp.task('sass:components', function () {
-    return gulp.src('./scss/components.scss')
+    return gulp.src(SRC_COMPONENTS)
         .pipe(sass().on('error', sass.logError))  
         .pipe(minify())
         .pipe(rename(`skittles-components-${json.version}#${new Date().getTime()}.min.css`))
-        .pipe(gulp.dest('./css'));
+        .pipe(gulp.dest(DEST));
 });
 
 gulp.task('sass:watch', function () {
-    gulp.watch('./scss/**/*.scss', ['sass']);
+    gulp.watch(SRC_ALL, ['sass']);
 });
+
+gulp.task('doc:assets', function() {
+    return gulp.src(['assets/highlight.pack.js', 'assets/styles/atom-one-dark.css'])
+        .pipe(copy('./build', {prefix:2}));
+});
+
+gulp.task('doc:run', run('dotnet run --project src\\doc\\Skittles.csproj'));
+
+gulp.task('prototype:serve', run('http-server ./ -o http://localhost:8080/src/prototype/index.html'));
+
+gulp.task('doc', ['sass', 'doc:assets', 'doc:run']);
+gulp.task('prototype', ['sass', 'prototype:serve']);
+
 
 gulp.task('default', ['sass', 'sass:watch']);
---
+++ src/doc/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+using System.Reflection;
+
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+
+namespace skittles
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseContentRoot(WebRootPath())
+                .UseWebRoot(WebRootPath())
+                .UseStartup<Startup>();
+
+        private static string WebRootPath() =>
+            Path.Combine(Path.GetDirectoryName(Assembly.GetEntryAssembly().Location), "..\\..");
+    }
+}
